47fd0fdcad011ad13a753418e459b73f348c167e
rutyak <132219103+rutyak@users.noreply.github.com>
2023-06-14 01:36:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ function hasKey(key) {
   return Object.prototype.hasOwnProperty.call(sampleObject, key);
 }
 
+// Example usage:
+console.log(hasKey("red"));    // true
+console.log(hasKey("blue"));   // false
+console.log(hasKey("white"));  // true
+
+
 // Do not change the code below
 const key = prompt("Enter Key.");
 alert(hasKey(key));
